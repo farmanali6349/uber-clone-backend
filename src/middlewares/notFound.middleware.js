fdd86769d7611e888835748596d76cc51ebf43fc
Farman Ali <farmanali6349@gmail.com>
2026-02-26 11:05:52 +0500

    resolved the critical bug at notFound middleware

--- src/middlewares/notFound.middleware.js
+++ src/middlewares/notFound.middleware.js
@@ -1,7 +1,7 @@
 import { ApiError } from '../utils/ApiError.util.js';
 
-const notFound = (err, req, res, next) => {
-  const error = ApiError.notFound(`Route ${req.originalUrl} Not Found`, err);
+const notFound = (req, res, next) => {
+  const error = ApiError.notFound(`Route ${req.originalUrl} Not Found`);
 
   next(error);
 };
